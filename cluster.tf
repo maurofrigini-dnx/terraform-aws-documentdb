886824190caeb938014ae642089e022639ee999f
Marcin Cuber <5611060+marcincuber@users.noreply.github.com>
2023-07-20 19:08:07 +0100

    fix cluster identifier

--- cluster.tf
+++ cluster.tf
@@ -1,7 +1,7 @@
 resource "aws_docdb_cluster_instance" "docdb" {
   count = var.instance_count
 
-  identifier_prefix = "${aws_docdb_cluster.docdb.cluster_identifier}-${count.index}-"
+  identifier_prefix = "${var.name_prefix}-${count.index}-"
 
   apply_immediately               = var.apply_immediately
   cluster_identifier              = aws_docdb_cluster.docdb.id
